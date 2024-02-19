d3a1b824a56cba3d61fff573d7b25311b86d7d67
Hunter Boles <hunterb9101@gmail.com>
2024-02-20 00:13:26 +1100

    feat: Future-proof current code

--- src/mltools/types.py
+++ src/mltools/types.py
@@ -5,8 +5,7 @@ import pydantic as pdt
 T = TypeVar("T")
 
 class TrainValTest(pdt.BaseModel, Generic[T]):
-    class Config:
-        arbitrary_types_allowed = True
+    model_config = pdt.ConfigDict(arbitrary_types_allowed=True)
     train: T
     val: T
     test: T
