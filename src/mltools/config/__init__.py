00feeb589bec23c59f93e43839e64021bef797a9
Hunter Boles <hunterb9101@gmail.com>
2026-05-01 14:50:53 -0500

    fix: MyPy errors

--- src/mltools/config/__init__.py
+++ src/mltools/config/__init__.py
@@ -23,50 +23,46 @@ class PathConfig(pdt.BaseModel):
     tag: str = "prod"
 
     @pdt.computed_field
-    @property
     def data_path(self) -> str:
         """Return the root data directory."""
         return str(Path(self.root_path) / "data")
 
     @pdt.computed_field
-    @property
     def raw_data_path(self) -> str:
         """Return the raw training data path.
 
         This should combine data sources down to a
         single file, but that does not mean that it is fully processed.
         """
-        return str(Path(self.data_path) / "raw" / "train.parquet")
+        return str(Path(self.root_path) / "data" / "raw" / "train.parquet")
 
     @pdt.computed_field
-    @property
     def processed_data_dir(self) -> str:
         """Return the processed data directory.
 
         These files should be split into train, val, and test,
         but still model-agnostic.
         """
-        return str(Path(self.data_path) / "processed")
+        return str(Path(self.root_path) / "data" / "processed")
 
     @pdt.computed_field
-    @property
     def processed_data_path(self) -> mt.TrainValTest[str]:
         """Return train, validation, and test processed data paths."""
+        processed_data_dir = Path(self.root_path) / "data" / "processed"
         return mt.TrainValTest(
-            train=str(Path(self.processed_data_dir) / "train.parquet"),
-            val=str(Path(self.processed_data_dir) / "val.parquet"),
-            test=str(Path(self.processed_data_dir) / "test.parquet"),
+            train=str(processed_data_dir / "train.parquet"),
+            val=str(processed_data_dir / "val.parquet"),
+            test=str(processed_data_dir / "test.parquet"),
         )
 
     @pdt.computed_field
-    @property
     def artifact_dir(self) -> str:
         """Return the model-agnostic artifact directory.
 
         Model-agnostic artifacts should be saved here, such as processing data
         pipelines.
         """
-        return str(Path(self.data_path) / "artifacts")
+        return str(Path(self.root_path) / "data" / "artifacts")
 
 
 class ModelPathConfig(pdt.BaseModel):
@@ -76,13 +72,11 @@ class ModelPathConfig(pdt.BaseModel):
     model: str
 
     @pdt.computed_field
-    @property
     def data_path(self) -> str:
         """Return the root data directory."""
         return str(Path(self.root_path) / "data")
 
     @pdt.computed_field
-    @property
     def dmatrix_dir(self) -> str:
         """Return the design matrix directory.
 
@@ -90,39 +84,36 @@ class ModelPathConfig(pdt.BaseModel):
         Any model-specific transformations should be performed here, such as
         feature selection, conversion to a Torch Dataset, etc.
         """
-        return str(Path(self.data_path) / "dmatrix")
+        return str(Path(self.root_path) / "data" / "dmatrix")
 
     @pdt.computed_field
-    @property
     def dmatrix_path(self) -> mt.TrainValTest[str]:
         """Return train, validation, and test design matrix paths."""
+        dmatrix_dir = Path(self.root_path) / "data" / "dmatrix"
         return mt.TrainValTest(
-            train=str(Path(self.dmatrix_dir) / f"train-{self.model}.parquet"),
-            val=str(Path(self.dmatrix_dir) / f"val-{self.model}.parquet"),
-            test=str(Path(self.dmatrix_dir) / f"test-{self.model}.parquet"),
+            train=str(dmatrix_dir / f"train-{self.model}.parquet"),
+            val=str(dmatrix_dir / f"val-{self.model}.parquet"),
+            test=str(dmatrix_dir / f"test-{self.model}.parquet"),
         )
 
     @pdt.computed_field
-    @property
     def feature_pipeline_path(self) -> str:
         """Return the feature pipeline artifact path.
 
         The feature selection and augmentation pipeline, a fit/transform class,
         should be saved as a pickle file here.
         """
-        return str(Path(self.model_dir) / f"fp-{self.model}.pkl")
+        return str(Path(self.root_path) / "models" / f"fp-{self.model}.pkl")
 
     @pdt.computed_field
-    @property
     def model_dir(self) -> str:
         """Return the model artifact directory."""
         return str(Path(self.root_path) / "models")
 
     @pdt.computed_field
-    @property
     def model_path(self) -> str:
-        """The model artifact should be saved here."""
-        return str(Path(self.model_dir) / f"{self.model}.pkl")
+        """Return the model artifact path."""
+        return str(Path(self.root_path) / "models" / f"{self.model}.pkl")
 
 
 class ModelConfig(pdt.BaseModel):
@@ -133,7 +124,6 @@ class ModelConfig(pdt.BaseModel):
     model: str = "gbm"
 
     @pdt.computed_field
-    @property
     def paths(self) -> ModelPathConfig:
         """Return model-specific path configuration."""
         return ModelPathConfig(root_path=self.root_path, model=self.model)
